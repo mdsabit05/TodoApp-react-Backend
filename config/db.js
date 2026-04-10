a00abe4a3b668ec5f3cbc752b0dc9d4139d89784
Md Sabit <72883223+mdsabit05@users.noreply.github.com>
2026-04-10 22:52:47 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 mongoose.connect(process.env.MONGO_URI, {
-  family: 4, // 🔥 THIS FIXES SRV DNS ISSUE
+  family: 4, 
 })
 .then(() => console.log("MongoDB Connected ✅"))
 .catch((err) => console.log("DB Error:", err));
